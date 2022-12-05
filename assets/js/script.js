dcd711805a6acdc5c734e684de4f6ee60182e9f1
alkasah <alkasah485@gmail.com>
2022-12-05 02:05:54 -0800

    updation in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,48 @@
-var counter = document.querySelector("#counter");
-var counter = document.querySelector("#counter");
-var counter = document.querySelector("#counter");
+var appendSeconds = document.querySelector("#seconds");
+var appendTens = document.querySelector("#tens")
+var startButton = document.querySelector("#start");
+var stopButton = document.querySelector("#stop");
+var resetButton = document.querySelector("#reset");
+var Interval ;
+var seconds = 00;
+var tens = 00;
+
+startButton.addEventListener("click", function(){
+    
+    clearInterval(Interval);
+    Interval = setInterval(startTimer,10);
+
+});
+
+function startTimer () {
+    tens++; 
+    if(tens <= 9){
+      appendTens.innerHTML = "0" + tens;
+    }
+    if (tens > 9){
+      appendTens.innerHTML = tens;
+    } 
+    if (tens > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+    if (seconds > 9){
+      appendSeconds.innerHTML = seconds;
+    }
+   }
+  
+
+   stopButton.onclick = function() {
+    clearInterval(Interval);
+}
+
+resetButton.onclick = function() {
+  clearInterval(Interval);
+ tens = "00";
+ seconds = "00";
+ appendTens.innerHTML = tens;
+ appendSeconds.innerHTML = seconds;
+}
